2a2a292dd33121a6bcb604a22afb010ab64c4919
Flameberry <flameberrydev@gmail.com>
2023-11-06 20:18:36 +0530

    Added support for static website creation using django-distill.

--- base/urls.py
+++ base/urls.py
@@ -4,10 +4,12 @@ from django.conf.urls.static import static
 
 from . import views
 
+from django_distill import distill_path
+
 urlpatterns = [
-    path("", views.home, name="home"),
+    distill_path("", views.home, name="home"),
+    distill_path("overview/", views.overview, name="overview"),
     path("login/", views.login_page, name="login"),
     path("logout/", views.logout_user, name="logout"),
     path("signup/", views.sign_up, name="signup"),
-    path("overview/", views.overview, name="overview"),
 ]
--- flameberry/settings.py
+++ flameberry/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "base",
+    "django_distill",
 ]
 
 MIDDLEWARE = [
@@ -117,6 +119,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "static_root"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
